44eeed081d047f1c949a867e717064d928a3a0d2
oscar <oscarmtzz22@gmail.com>
2020-11-18 08:47:32 +0100

    feat consume pulso eliminando del array de pulsos acumulados

--- src/routes/pins/pins.service.js
+++ src/routes/pins/pins.service.js
@@ -1,37 +1,31 @@
 const TareaNoSQLWebservice = require('../../lib/repository/tareaNoSQL.ws')
-const device = process.env.DEVICE_ENV ||'raspi'
+const device = process.env.DEVICE_ENV || 'raspi'
 let GpioConfiguracion = null
-if(device == 'raspi'){
+if (device == 'raspi') {
     GpioConfiguracion = require('../../lib/pins/gpio.config')
 }
 
 function PinsService() {
 
     async function obtenerEstadoPins(req, res, next) {
-        if(device == 'raspi'){
+        if (device == 'raspi') {
             res.json(GpioConfiguracion.PINS)
         }
-        else{
-            /*res.json({
-                "GPIO14":{
-                    flanco :'up',
-                    mode: 'in',
-                    type: 'main-pulse',
-                    status: 'on',
-                    pulsesUp:[1,1]
-                }
-            })*/
+        else {
             res.json({})
         }
     }
 
-    async function registrarPulso(req,res, next){
-        const {IdPuesto, ProductoPorPulso} = req.body
+    async function registrarPulso(req, res, next) {
+        const { IdPuesto, PinPulso, ProductoPorPulso } = req.body
         try {
-            const msg = await TareaNoSQLWebservice.sumarPares(IdPuesto,ProductoPorPulso)
+            const msg = await TareaNoSQLWebservice.sumarPares(IdPuesto, ProductoPorPulso)
+            if (device == 'raspi') {
+                GpioConfiguracion.PINS[PinPulso].pulsesUp.pop()
+            }
             return res.json(msg)
         } catch (error) {
-            return res.status(500).json({message: String(error)})
+            return res.status(500).json({ message: String(error) })
         }
     }
 
